b252d309fba23450115d6949371148dc8895768b
Daiki Saito <daiki.saito92@gmail.com>
2021-01-07 14:51:49 +1000

    add number input with thousand separator

---
+++ src/components/common/input/number-input/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NumberFormat from 'react-number-format';
+import './index.scss';
+
+function NumberInput({ label, value, name, placeholder, disabled, onChange }) {
+  return (
+    <div className="custom-number-input">
+      {label.length > 0 && <label htmlFor="custom-number-input">{label}</label>}
+      <NumberFormat
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        disabled={disabled}
+        thousandSeparator
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+NumberInput.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+NumberInput.defaultProps = {
+  label: '',
+  value: '',
+  placeholder: '',
+  disabled: false,
+  onChange: () => {},
+};
+
+export default NumberInput;
